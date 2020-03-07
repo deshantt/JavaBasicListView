37244721ca3a74f542fb363a1d482e5a24bed1a9
deshantt <dewishanty.workspace@gmail.com>
2020-03-07 13:23:01 +0700

    implement simple ListView

--- app/src/main/java/com/deshantt/javabasiclistview/MainActivity.java
+++ app/src/main/java/com/deshantt/javabasiclistview/MainActivity.java
@@ -3,10 +3,13 @@ package com.deshantt.javabasiclistview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
     private ListView list_color;
     private String[] colors =   {"red", "blue", "yellow", "green", "purple",
                                 "red", "blue", "yellow", "green", "purple",
@@ -21,5 +24,10 @@ public class MainActivity extends AppCompatActivity {
         list_color = findViewById(R.id.lv_color);
         ArrayAdapter<String> myAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, colors);
         list_color.setAdapter(myAdapter);
+        list_color.setOnItemClickListener(this);
+    }
+
+    public void onItemClick(AdapterView<?> arg0, View arg1, int pos, long args3){
+        Toast.makeText(this, "This toast shows the "+colors[pos]+ " value.", Toast.LENGTH_SHORT).show();
     }
 }
